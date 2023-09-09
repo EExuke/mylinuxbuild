8f0e70c8778e11d692e94c8a72bce4b8788084ff
xuke <xuke@inhand.com.cn>
2023-09-09 21:58:11 +0800

    fix: 优化myfy.sh脚本, 兼容支持有道和百度的API,并完成输出格式化调整;

--- bin/myfy.sh
+++ bin/myfy.sh
@@ -28,43 +28,89 @@ B_YL="\033[43m"   B_BU="\033[44m"   B_PR="\033[45m"
 B_DG="\033[46m"   B_WT="\033[47m"
 
 #-----------------------------------------------------------
-#                  Keys
+#                  API srcourse config
 #-----------------------------------------------------------
-API_ADDR="https://fanyi-api.baidu.com/api/trans/vip/translate"
-APP_ID="20230908001809652"
-APP_KEY="Blsc9e5yvpHLO74JBztT"
+#API_SRC="baidu"
+API_SRC="youdao"
 
-#HEADER="Content-Type:application/json"
+#-----------------------------------------------------------
+#                  参数定义
+#-----------------------------------------------------------
 HEADER="Content-Type:application/x-www-form-urlencoded"
-SALT=`date +%s`
+HEADER_JSON="Content-Type:application/json"
+CUR_TIME=`date +%s`
+SALT=`uuid`
 INPUT_STR="${*}"
+
+# 输入判断
+if [[ ${INPUT_STR} == "" ]]; then
+	echo -e "${F_GR}Usage: fy <text|文本> ${C_ED}"
+	exit 0
+fi
 
 # 转URL编码
 input_ec=`echo -n ${INPUT_STR} | od -An -tx1 | tr -d '\n' | tr ' ' %`
 
-#-----------------------------------------------------------
-#                  MAIN PROCESS
-#-----------------------------------------------------------
-#sign=MD5(appid + q + salt + 密钥)
-sign=`echo -n "${APP_ID}${INPUT_STR}${SALT}${APP_KEY}" | md5sum | awk '{print $1}'`
-
 # 判断中英文
 echo ${INPUT_STR} | grep -qP '[^\x00-\x7F]'
 if [[ $? -eq 0 ]]; then
-	# zh to en
-	data="q=${input_ec}&from=zh&to=en&appid=${APP_ID}&salt=${SALT}&sign=${sign}&action=0"
+	from="zh"
+	to="en"
 else
-	# en to zh
-	data="q=${input_ec}&from=en&to=zh&appid=${APP_ID}&salt=${SALT}&sign=${sign}&action=0"
+	from="en"
+	to="zh"
+fi
+
+if [[ ${API_SRC} == "baidu" ]]; then
+	#-----------------------------------------------------------
+	#                  baidu Keys
+	#-----------------------------------------------------------
+	API_ADDR="https://fanyi-api.baidu.com/api/trans/vip/translate"
+	APP_ID="20230908001809652"
+	APP_KEY="Blsc9e5yvpHLO74JBztT"
+
+	#-----------------------------------------------------------
+	#                  百度参数组织
+	#-----------------------------------------------------------
+	#sign=MD5(appid + q + salt + 密钥)
+	sign=`echo -n "${APP_ID}${INPUT_STR}${SALT}${APP_KEY}" | md5sum | awk '{print $1}'`
+
+	data="q=${input_ec}&from=${from}&to=${to}&appid=${APP_ID}&salt=${SALT}&sign=${sign}"
+elif [[ ${API_SRC} == "youdao" ]]; then
+	#-----------------------------------------------------------
+	#                  youdao Keys
+	#-----------------------------------------------------------
+	API_ADDR="https://openapi.youdao.com/api"
+	APP_ID="6ad7ad0f6eb1a626"
+	APP_KEY="XY1mSTnjn9CYu1eyvt8Elh2pRugczLzR"
+
+	#-----------------------------------------------------------
+	#                  有道参数组织
+	#-----------------------------------------------------------
+	#sign=sha256(应用ID + input + salt + curtime + 应用密钥)；
+	#TODO: input的计算方式为：input=q前10个字符 + q长度 + q后10个字符（当q长度大于20）或 input=q字符串（当q长度小于等于20）；
+	sign=`echo -n "${APP_ID}${INPUT_STR}${SALT}${CUR_TIME}${APP_KEY}" | sha256sum | awk '{print $1}'`
+	signType="v3"
+	from="auto"
+
+	data="q=${input_ec}&from=${from}&to=${to}&appKey=${APP_ID}&salt=${SALT}&sign=${sign}&signType=${signType}&curtime=${CUR_TIME}"
 fi
 
 #-----------------------------------------------------------
-#                   run curl
+#                   Run curl
 #-----------------------------------------------------------
 result=`curl -s ${API_ADDR} -H ${HEADER} -d ${data}`
 
 #-----------------------------------------------------------
-#                   result parase
+#                   Result analysis
 #-----------------------------------------------------------
-echo ${result} | jq .trans_result[0]
+if [[ ${API_SRC} == "baidu" ]]; then
+	echo ${result} | jq .trans_result[0]
+elif [[ ${API_SRC} == "youdao" ]]; then
+	if [[ ${to} == "en" ]]; then
+		echo ${result} | jq -c .translation,.basic.explains,.web[]
+	else
+		echo ${result} | jq -c .translation,.basic.explains,.web[],.basic.wfs[].wf
+	fi
+fi
 
